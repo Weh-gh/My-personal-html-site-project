bd62cfe3fccda312dc3a1d04e9c3422dd8346443
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-03-25 12:36:08 +0300

    Switched to Expressjs. The codes may not be working properly.

--- server.js
+++ server.js
@@ -6,34 +6,18 @@ const hbs = require('express-hbs');
 const html = require('html');
 const php = require('php');
 const ejs = require('ejs');
-app.use(express.static(path.join(__dirname, 'styles.csss')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.render('home.html');
+app.get('/test3', (req, res) => {
+    res.render('test3.html');
 })
 app.get('/projects', (req, res) => {
     res.render('projects.html');
 })
 app.get('/shop', (req, res) => {
     res.render('shop.html');
-})
-app.get('/quotes', (req, res) => {
-    res.render('quotes.html');
-})
-app.get('/topics/linux', (req, res) => {
-    res.render('topics/linux.html');
-})
-app.get('/test', (req, res) => {
-    res.render('test.html');
-})
-app.get('/404', (req, res) => {
-    res.redirect('404.html');
-})
-app.get('*', (req, res) => {
-    var noWay = req.url;
-    res.render('404.html', { noWay });
 })
 
 // Launching the site.
