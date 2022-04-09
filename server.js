4b48f5825e0f4c2de99e89eae0178e99e558e791
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-04-09 09:39:06 +0300

    SOURCES

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const html = require('html');
 const php = require('php');
 const ejs = require('ejs');
 const include = require('./include.json');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs')
 
